b45cd3d63bfc5a9d4caf6a6ae46e24ced18ff05c
Rishon73 <sshiff@gmail.com>
2018-01-11 17:44:13 -0500

    first commits

---
+++ set-lft-conf-env-var-Mac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters."
+    echo "Expecting a full path to a json config file"
+    exit
+fi
+
+
+if [[ $(uname -s) == Linux ]]
+then
+    echo "--- This script only works on Mac."
+    echo "--- To set evn var on Linux, use:"
+    echo "--- export LFT_TEST_CONFIG=<FULL_PATH_TO_CONF_FILE>"
+    exit
+else
+    launchctl setenv LFT_TEST_CONFIG $1
+    echo "LFT_TEST_CONFIG environment variable is set to: " $(launchctl getenv LFT_TEST_CONFIG)
+    #launchctl unsetenv LFT_TEST_CONFIG
+fi
